fa8ed9a195eba1fd4207a3f9a9b02f13605e2b29
Johan <johan@dospuntos.no>
2023-09-19 13:51:47 +0000

    Working MainWindow

--- MainWindow.h
+++ MainWindow.h
@@ -1,16 +1,20 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <TextView.h>
 #include <Window.h>
+#include "FortuneFunctions.h"
 
 class MainWindow : public BWindow
 {
 public:
 						MainWindow(void);
 			void		MessageReceived(BMessage *msg);
-			bool		QuitRequested(void);
+			void		FrameResized(float w, float h);
 			
 private:
+	BTextView			*fTextView;
+	FortuneAccess		fFortune;
 };
 
 #endif
